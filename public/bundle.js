e1153f7ab757e1ed40747176b10e7d7872180158
Eduardo Puermas <eduardopuermas@hotmail.com>
2019-10-26 15:59:49 +0000

    Initial Push

--- public/bundle.js
+++ public/bundle.js
@@ -701,44 +701,47 @@ __webpack_require__.r(__webpack_exports__);
  *  This function is called every time the user changes types or changes any input
  */
 function render(variables = {}) {
-  // here we ask the logical questions to make decitions on how to build the heml
+  // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+
+  //   if (variables.name != null) {
+  //   }
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/Eddy_Prms"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/epuermas"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/eduardopuermas8698/"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://www.instagram.com/eddy_prms/"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
 }
 
 /**
- * Ignore this lines below, here is where we do the logic for the dropdowns
+ * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
 window.onload = function () {
   window.variables = {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background: "https://images.unsplash.com/photo-1495954484750-af469f2f9be5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "https://media.licdn.com/dms/image/C4E03AQE8X5hjLqNqlQ/profile-displayphoto-shrink_200_200/0?e=1577318400&v=beta&t=izt7iaWsWr6Zp0PT9P236NCPFKuKGg6Xae_T_2MwRmo",
     // social media bar position (left or right)
     socialMediaPosition: "left",
     // social media usernames
-    twitter: null,
-    github: "alesanchezr",
+    twitter: "Eddy_Prms",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
